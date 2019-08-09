88c1168b2ab044ad70d9d583ef87adea5d5c82d3
Matheus2001 <35998667+Matheus2001@users.noreply.github.com>
2019-08-09 15:24:24 -0300

    Update Militar.java

--- Militar.java
+++ Militar.java
@@ -1,1 +1,38 @@
+abstract class Militar {
+  private String patente;
+  private int matricula;
+  
 
+  public Militar(String patente, int matricula){
+    this.patente = patente;
+    this.matricula = matricula;
+  }
+  public String getpatente(){
+    return patente;
+  }
+  public int getMatricula(){
+    return matricula;
+  }
+  
+
+  public abstract boolean podeProgredir();
+}
+
+class MilitarAeronautica extends Militar{
+  private double duraPatente;
+  private double horaVoo;
+
+  public MilitarAeronautica(double duraPatente, double horaVoo,String patente, int matricula){
+    super (patente, matricula);
+    this.duraPatente = duraPatente;
+    this.horaVoo = horaVoo;
+  }
+  public boolean podeProgredir(){
+    if (duraPatente > 2 && horaVoo > 100){
+      return true;
+    }else {
+      return false;
+    }
+  }
+}
+class MilitarExercito
